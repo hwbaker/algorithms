bda41c7c8f2c132fc6b0220e234ed4f28adfc38b
hewei <hewei860311@sohu.com>
2017-02-06 14:59:27 +0800

    select

--- selectSort.php
+++ selectSort.php
@@ -5,15 +5,18 @@
  * User: hewei
  * Date: 16/12/30
  * Time: 下午4:46
+ *
+ * cd /users/hewei/site/git/algorithms
+ * /usr/local/bin/php selectSort.php num 200
  */
-echo "<pre>";
-$time1 = time();
-$arr = generateRandomArray(100, 1, 100000);
-echo 'before:' . print_r($arr, true);
+
+require("common.php");
+$common = new common();
+$num = isset($argv[1]) && $argv[1] == 'num' && isset($argv[2]) ? $argv[2] : 10; // 数组大小
+$arr = $common->generateRandomArray($num, 1, 100000);
+//echo 'before:' . print_r($arr, true);
+$timeSta = $common->getMillisecond();
 $n = count($arr);
-$time2 = time();
-$diff = $time2 - $time1;
-echo 'diff:' . $diff . "s\n\r";
 
 for ($i = 0; $i < $n; $i++) {
     for ($j = $i + 1; $j < $n; $j++) {
@@ -24,28 +27,5 @@ for ($i = 0; $i < $n; $i++) {
         }
     }
 }
-
-$time3 = time();
-$diff = $time3 - $time2;
-echo 'diff:' . $diff . "s\n\r";
-echo 'after:' . print_r($arr, true);
-
-/**
- * @desc 随机生成算法测试用例,生成n个元素的随机数,每个元素的随机范围是[rangeL,rangeR]
- * @param $n      数组元素个数
- * @param $rangeL 数组左范围
- * @param $rangeR 数组右范围
- * @return array
- */
-function generateRandomArray($n, $rangeL, $rangeR)
-{
-    $count = 0;
-    $return = array();
-    while ($count < $n) {
-        $return[] = mt_rand($rangeL, $rangeR);
-        $return = array_flip(array_flip($return));
-        $count = count($return);
-    }
-    shuffle($return);
-    return $return;
-}
+echo $common->timeDiff($timeSta);
+//echo 'after:' . print_r($arr, true);
