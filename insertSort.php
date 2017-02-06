608029a4bc7ab072c46d1b59e26cbcc69982a67b
hewei <hewei860311@sohu.com>
2017-02-06 14:50:12 +0800

    insert

--- insertSort.php
+++ insertSort.php
@@ -5,77 +5,86 @@
  * User: hewei
  * Date: 17/1/4
  * Time: 下午6:02
+ *
+ * cd /users/hewei/site/git/algorithms
+ * /usr/local/bin/php insertSort.php num 100
  */
+require("common.php");
 
 /**
- * @desc 随机生成算法测试用例,生成n个元素的随机数,每个元素的随机范围是[rangeL,rangeR]
- * @param $n
- * @param $rangeL
- * @param $rangeR
- * @return array
+ * @desc 插入排序未优化
+ * @param array $arr
  */
-function generateRandomArray($n, $rangeL, $rangeR)
+function insertSortComplex(array &$arr)
 {
-    $count = 0;
-    $result = array();
-    while ($count < $n) {
-        $result[] = mt_rand($rangeL, $rangeR);
-        $result = array_flip(array_flip($result));
-        $count = count($result);
-    }
-    shuffle($result);
-    return $result;
-}
-
-$arr = generateRandomArray(10, 1, 500);
-echo "<pre>";
-echo 'before:' . print_r($arr, true);
-$n = count($arr);
-// 插入排序算法
-//for ($i = 1; $i < $n; $i++ ) {
-//    for ($j = $i; $j > 0; $j--) {
-//        if ($arr[$j] < $arr[$j-1]) {
-//            $tmp = $arr[$j];
-//            $arr[$j] = $arr[$j-1];
-//            $arr[$j-1] = $tmp;
-//        } else {
-//            break; // 不满足条件提前退出
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        echo 'i:' . $i . ';';
+//        for ($j = $i; $j > 0; $j--) {
+//            echo 'j:' . $j . ' ';
+//            if ($arr[$j-1] > $arr[$j]) {
+//                $tmp = $arr[$j];
+//                $arr[$j] = $arr[$j-1];
+//                $arr[$j-1] = $tmp;
+//            } else {
+//                break; //不满足交换条件,退出内层循环
+//            }
 //        }
-//        echo 'i:' . $i .';'.'j:' . $j .';'.print_r($arr, true);
-//    }
-//}
-
-// 简化写法
-//for ($i = 1; $i < $n; $i++) {
-//    for ($j = $i; $j> 0 && $arr[$j] < $arr[$j-1]; $j--) { // && 关联同时满足
-//        $tmp = $arr[$j];
-//        $arr[$j] = $arr[$j-1];
-//        $arr[$j-1] = $tmp;
-//    }
-//}
-
-//插入算法的改进:第二层循环的"3步交换"比赋值更加耗费性能,因此简化"交换"操作
-for ($i = 1; $i < $n; $i++) {
-    // 寻找arr[i]元素合适的位置
-    $element = $arr[$i];
-    // j保存元素element应该插入的位置
-    for ($j = $i; $j > 0 && $arr[$j-1] > $element; $j--) {
-        $arr[$j] = $arr[$j-1];
+        // 简化写法
+        for ($j = $i; $j > 0 && $arr[$j-1] > $arr[$j]; $j--) { // && $arr[$j-1] > $arr[$j],&&关联符号满足交换条件
+            echo 'j:' . $j . ' ';
+            $tmp = $arr[$j];
+            $arr[$j] = $arr[$j-1];
+            $arr[$j-1] = $tmp;
+        }
+        echo "\r\n";
     }
-    $arr[$j] = $element;
-}
-echo 'after:' . print_r($arr, true);
 
-//内层循环while形式
-for ($i = 1; $i < $n; $i++) {
-    // 寻找arr[i]元素合适的位置
-    $element = $arr[$i];
-    // j保存元素element应该插入的位置
-    $j = $i;
-    while ($j > 0 && $arr[$j-1] > $element) {
-        $arr[$j] = $arr[$j-1];
-        $j--;
-    }
-    $arr[$j] = $element;
 }
-echo 'after:' . print_r($arr, true);
+
+/**
+ * @desc 插入排序算法,改进版.第二层循环的"3步交换"比赋值更加耗费性能,因此简化"交换"操作
+ * @param array $arr
+ */
+function insertSortAdv(array &$arr)
+{
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        // 寻找arr[i]元素合适的位置
+        $element = $arr[$i];
+        // j保存元素element应该插入的位置
+        $j = $i;
+        for ($j; $j > 0 && $arr[$j-1] > $element; $j--) {
+            // j元素值依次向后移一位
+            $arr[$j] = $arr[$j-1];
+        }
+        $arr[$j] = $element;
+    }
+}
+
+/**
+ * @desc 插入排序算法,改进版.内层循环while形式
+ * @param array $arr
+ */
+function insertSortAdvWhile(array &$arr)
+{
+    $n = count($arr);
+    for ($i = 1; $i < $n; $i++) {
+        $element = $arr[$i];
+        $j = $i;
+        while ($j > 0 && $arr[$j-1] > $element) {
+            $arr[$j] = $arr[$j-1];
+            $j--;
+        }
+        $arr[$j] = $element;
+    }
+}
+
+$common = new common;
+$num = isset($argv[1]) && $argv[1] == 'num' && isset($argv[2]) ? $argv[2] : 10; //数组大小
+$arr = $common->generateRandomArray($num, 1, 2000);
+$timeSta = $common->getMillisecond();
+//echo 'before:' . implode(',', $arr) . "\r\n";
+insertSortAdvWhile($arr);
+echo  $common->timeDiff($timeSta);
+//echo 'after:' . implode(',', $arr) . "\r\n";
