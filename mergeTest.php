18958bb5d33c22e1ea8266ffa4ea761cb6fec00e
hewei <hewei860311@sohu.com>
2017-04-25 11:26:16 +0800

    mergeTest

---
+++ mergeTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hewei
+ * Date: 17/4/20
+ * Time: 下午6:32
+ * cd /users/hewei/site/git/algorithms
+ * /usr/local/bin/php mergeTest.php
+ */
+
+//计数num
+$num = 0;
+
+function mergeSort(array &$array)
+{
+    $n = count($array);
+    __mergeSort($array, 0, $n-1);
+}
+
+function __mergeSort(array &$array, $l, $r)
+{
+    if ($l >= $r ) {
+//        echo 'l >= r:' ."$l >= $r \r\n";
+        return false;
+    }
+
+    $mid = floor(($l + $r)/2);
+    __mergeSort($array, $l, $mid);
+    __mergeSort($array, $mid + 1, $r);
+    __merge($array, $l, $mid, $r);
+}
+
+function __merge(array &$array, $l, $mid , $r)
+{
+    global $num;
+    for ($i = $l; $i <= $r; $i++) {
+        $aux[$i - $l] = $array[$i];
+    }
+//    echo 'aux:'.print_r($aux, true);
+
+    $i = $l;
+    $j = $mid +1;
+    $k = $l;
+    for ($k; $k <= $r; $k++) {
+        if ($i > $mid) {
+            $array[$k] = $aux[$j - $l];
+            $num += ($mid - $i + 1);
+            $j++;
+        } elseif ($j > $r) {
+            $array[$k] = $aux[$i - $l];
+            $i++;
+        } elseif ($aux[$i - $l] > $aux[$j - $l] ) {
+            $array[$k] = $aux[$j - $l];
+            $num += ($mid - $i + 1);
+            $j++;
+        } else {
+            $array[$k] = $aux[$i - $l];
+            $i++;
+        }
+    }
+    unset($aux);
+//    echo 'num:'.$num."\r\n";
+}
+
+$arr = array(1,2,3,4,5,6);
+echo 'before:' . print_r($arr, true);
+mergeSort($arr);
+echo 'after:' . print_r($arr, true);
+echo 'invertedPair:'.$num."\r\n";
