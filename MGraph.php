a885e22049f5dd34f588465a05f56d92dc7d8aa0
hewei <hewei860311@sohu.com>
2017-07-06 16:31:02 +0800

    up

--- MGraph.php
+++ MGraph.php
@@ -16,8 +16,8 @@
  */
 class MGraph{
     private $vertexArray; // 顶点数组
-    private $arc; // 边邻接矩阵,即二维数组
-    private $arcData; // 边的数组信息
+    private $graph; // 边邻接矩阵,即二维数组
+    private $graphData; // 边的数组信息
     private $direct; // 图的类型(无向或有向):0:无向 ; 1:有向
     private $hasList; // 尝试遍历时存储遍历过的结点
     private $queue; // 广度优先遍历时存储孩子结点的队列,用数组模仿
@@ -26,23 +26,23 @@ class MGraph{
     private $primArc; // prim算法时保存边
     private $krus;// kruscal算法时保存边的信息
 
-    public function __construct($vertexArray, $arc, $direct = 0){
+    public function __construct($vertexArray, $graph, $direct = 0){
         $this->vertexArray = $vertexArray;
-        $this->arcData = $arc;
+        $this->graphData = $graph;
         $this->direct = $direct;
         // 初始化图
-        $this->initializeArc();
+        $this->initializeGraph();
         // 创建图
-        $this->createArc();
+        $this->createGraph();
     }
 
     /**
      * @desc 初始化图[邻接矩阵]
      */
-    private function initializeArc(){
+    private function initializeGraph(){
         foreach($this->vertexArray as $value){
             foreach($this->vertexArray as $cValue){
-                $this->arc[$value][$cValue] = ($value == $cValue ? 0 : $this->infinity);
+                $this->graph[$value][$cValue] = ($value == $cValue ? 0 : $this->infinity);
             }
         }
     }
@@ -51,29 +51,29 @@ class MGraph{
      * @desc 创建图
      * $direct 0:无向图 ; 1:有向图
      */
-    private function createArc(){
-        foreach($this->arcData as $key=>$value){
+    private function createGraph(){
+        foreach($this->graphData as $key=>$value){
             $strArr = str_split($key);
             $first = $strArr[0];
             $last = $strArr[1];
-            $this->arc[$first][$last] = $value;
+            $this->graph[$first][$last] = $value;
 
             if(!$this->direct){
-                $this->arc[$last][$first] = $value;
+                $this->graph[$last][$first] = $value;
             }
         }
     }
 
     /**
-     * @desc floyd算法[佛洛依德算法],主要是在顶点集内,按点与点相邻边的权重做遍历,
-     * 如果两点不相连则权重无穷大,这样通过多次遍历可以得到点到点的最短路径,
+     * @desc 最短路径:floyd算法[佛洛依德算法]
+     * 主要是在顶点集内,按点与点相邻边的权重做遍历,如果两点不相连则权重无穷大,这样通过多次遍历可以得到点到点的最短路径,
      * 逻辑上最好理解,实现也较为简单,时间复杂度为O(n^3);
      * @return array
      */
     public function floyd(){
         $path = array();//路径数组
         $distance = array();//距离数组
-        foreach($this->arc as $key=>$value){
+        foreach($this->graph as $key=>$value){
             foreach($value as $k=>$v){
                 $path[$key][$k] = $k;
                 $distance[$key][$k] = $v;
@@ -89,12 +89,12 @@ class MGraph{
                 }
             }
         }
-        return array($path, $distance);
+        return array('path' => $path, 'distance' => $distance);
     }
 
     /**
-     * @desc dijkstra算法[迪杰斯特拉算法],OSPF中实现最短路由所用到的经典算法,
-     * djisktra算法的本质是贪心算法,不断的遍历扩充顶点路径集合S,一旦发现更短的点到点路径就替换S中原有的最短路径,完成所有遍历后S便是所有顶点的最短路径集合了.
+     * @desc 最短路径:dijkstra算法[迪杰斯特拉算法],OSPF中实现最短路由所用到的经典算法,
+     * 其算法的本质是贪心算法,不断的遍历扩充顶点路径集合S,一旦发现更短的点到点路径就替换S中原有的最短路径,完成所有遍历后S便是所有顶点的最短路径集合了.
      * 时间复杂度为O(n^2)
      * @return array
      */
@@ -102,7 +102,7 @@ class MGraph{
         $final = array();
         $pre = array();//要查找的结点的前一个结点数组
         $weight = array();//权值和数组
-        foreach($this->arc[$this->vertexArray[0]] as $k=>$v){
+        foreach($this->graph[$this->vertexArray[0]] as $k=>$v){
             $final[$k] = 0;
             $pre[$k] = $this->vertexArray[0];
             $weight[$k] = $v;
@@ -121,9 +121,9 @@ class MGraph{
             $final[$key] = 1;
             for($j = 0; $j < count($this->vertexArray); $j ++){
                 $temp = $this->vertexArray[$j];
-                if($final[$temp] != 1 && ($min + $this->arc[$key][$temp]) < $weight[$temp]){
+                if($final[$temp] != 1 && ($min + $this->graph[$key][$temp]) < $weight[$temp]){
                     $pre[$temp] = $key;
-                    $weight[$temp] = $min + $this->arc[$key][$temp];
+                    $weight[$temp] = $min + $this->graph[$key][$temp];
                 }
             }
         }
@@ -131,7 +131,8 @@ class MGraph{
     }
 
     /**
-     * kruskal算法[克鲁斯卡尔算法],在图内构造最小生成树,达到图中所有顶点联通.从而得到最短路径.
+     * @desc 最短路径:kruskal算法[克鲁斯卡尔算法]
+     * 在图内构造最小生成树,达到图中所有顶点联通,从而得到最短路径
      * 时间复杂度为O(N*logN)
      */
     private function kruskal(){
@@ -139,7 +140,7 @@ class MGraph{
         foreach($this->vertexArray as $value){
             $krus[$value] = 0;
         }
-        foreach($this->arc as $key=>$value){
+        foreach($this->graph as $key=>$value){
             $begin = $this->findRoot($key);
             foreach($value as $k=>$v){
                 $end = $this->findRoot($k);
@@ -163,14 +164,14 @@ class MGraph{
     }
 
     /**
-     * @desc prim算法[普里姆算法],生成最小生成树
+     * @desc 最小生成树:prim算法[普里姆算法]
      * @return array
      */
     public function prim(){
         $this->primVertex = array();
         $this->primArc = array($this->vertexArray[0]=>0);
         for($i = 1; $i < count($this->vertexArray); $i ++){
-            $this->primArc[$this->vertexArray[$i]] = $this->arc[$this->vertexArray[0]][$this->vertexArray[$i]];
+            $this->primArc[$this->vertexArray[$i]] = $this->graph[$this->vertexArray[0]][$this->vertexArray[$i]];
             $this->primVertex[$this->vertexArray[$i]] = $this->vertexArray[0];
         }
         for($i = 0; $i < count($this->vertexArray); $i ++){
@@ -183,7 +184,7 @@ class MGraph{
                 }
             }
             $this->primArc[$key] = 0;
-            foreach($this->arc[$key] as $k=>$v){
+            foreach($this->graph[$key] as $k=>$v){
                 if($this->primArc[$k] != 0 && $v < $this->primArc[$k]){
                     $this->primArc[$k] = $v;
                     $this->primVertex[$k] = $key;
@@ -194,18 +195,18 @@ class MGraph{
     }
 
     /**
-     * @desc 一般算法,生成最小生成树
+     * @desc 最小生成树:一般算法
      * @return array
      */
     public function bst(){
         $this->primVertex = array($this->vertexArray[0]);
         $this->primArc = array();
-        next($this->arc[key($this->arc)]);
+        next($this->graph[key($this->graph)]);
         $key = NULL;
         $current = NULL;
         while(count($this->primVertex) < count($this->vertexArray)){
             foreach($this->primVertex as $value){
-                foreach($this->arc[$value] as $k=>$v){
+                foreach($this->graph[$value] as $k=>$v){
                     if(!in_array($k, $this->primVertex) && $v != 0 && $v != $this->infinity){
                         if($key == NULL || $v < current($current)){
                             $key = $k;
@@ -219,16 +220,16 @@ class MGraph{
             $key = NULL;
             $current = NULL;
         }
-        return array('vertexArray'=>$this->primVertex, 'arc'=>$this->primArc);
+        return array('vertexArray'=>$this->primVertex, 'graph'=>$this->primArc);
     }
 
     /**
-     * @desc 一般遍历
+     * @desc 遍历:一般遍历
      * @return string
      */
     public function reserve(){
         $this->hasList = array();
-        foreach($this->arc as $key=>$value){
+        foreach($this->graph as $key=>$value){
             if(!in_array($key, $this->hasList)){
                 $this->hasList[] = $key;
             }
@@ -246,13 +247,13 @@ class MGraph{
     }
 
     /**
-     * @desc 广度优先遍历 broad first
+     * @desc 遍历:广度优先遍历 broad first spanning
      * @return string
      */
     public function bfs(){
         $this->hasList = array();
         $this->queue = array();
-        foreach($this->arc as $key => $value){
+        foreach($this->graph as $key => $value){
             if(!in_array($key, $this->hasList)){
                 $this->hasList[] = $key;
                 $this->queue[] = $value;
@@ -261,7 +262,7 @@ class MGraph{
                     foreach($child as $k=>$v){
                         if($v == 1 && !in_array($k, $this->hasList)){
                             $this->hasList[] = $k;
-                            $this->queue[] = $this->arc[$k];
+                            $this->queue[] = $this->graph[$k];
                         }
                     }
                 }
@@ -271,44 +272,44 @@ class MGraph{
     }
 
     /**
-     * @desc 执行深度优先遍历
-     * @param $key
-     */
-    public function excuteDfs($key){
-        $this->hasList[] = $key;
-        foreach($this->arc[$key] as $k=>$v){
-            if($v == 1 && !in_array($k, $this->hasList)) {
-                $this->excuteDfs($k);
-            }
-        }
-    }
-
-    /**
-     * @desc 深度优先遍历 depth first
+     * @desc 遍历:深度优先遍历 depth first spanning
      * @return string
      */
     public function dfs(){
         $this->hasList = array();
         foreach($this->vertexArray as $key){
             if(!in_array($key, $this->hasList))
-                $this->excuteDfs($key);
+                $this->executeDfs($key);
         }
         return implode($this->hasList);
     }
 
     /**
-     * @desc 图的二维数组表示
+     * @desc 执行深度优先遍历
+     * @param $key
+     */
+    public function executeDfs($key){
+        $this->hasList[] = $key;
+        foreach($this->graph[$key] as $k=>$v){
+            if($v == 1 && !in_array($k, $this->hasList)) {
+                $this->executeDfs($k);
+            }
+        }
+    }
+
+    /**
+     * @desc 图的表示.二维数组矩阵
      * @return mixed
      */
     public function showArc(){
-//        return $this->arc;
+//        return $this->graph;
         echo '  ';
-        foreach ($this->arc as $vertexFather => $edgeFather) {
+        foreach ($this->graph as $vertexFather => $edgeFather) {
             echo str_pad($vertexFather, 6);
         }
         echo "\r\n";
 
-        foreach ($this->arc as $vertexFather => $edgeFather) {
+        foreach ($this->graph as $vertexFather => $edgeFather) {
             echo $vertexFather . ' ';
             foreach ($edgeFather as $vertex => $edge) {
                 echo str_pad($edge, 6);
@@ -324,20 +325,12 @@ class MGraph{
     public function getVertex(){
         return $this->vertexArray;
     }
-
-    /**
-     * @desc 返回结点个数
-     * @return int
-     */
-    public function getVexCount(){
-        return count($this->vertexArray);
-    }
 }
 
 // 顶点
 $vertex = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i');
 // 键为边, 值权值
-$arc = array(
+$graph = array(
     'ab'=>'10', 'af'=>'11',
     'bg'=>'16', 'bc'=>'18', 'bi'=>'12',
     'ci'=>'8', 'cd'=>'22',
@@ -346,7 +339,13 @@ $arc = array(
     'fg'=>'17', 'fe'=>'26',
     'gh'=>'19',
 );
-$MGraph = new MGraph($vertex, $arc);
+$MGraph = new MGraph($vertex, $graph);
 echo  $MGraph->showArc();
-echo  '顶点表示:' . implode( ' ', $MGraph->getVertex()) . "\r\n";
-//print_r($MGraph->dfs());
+echo  '顶点:' . implode( ' ', $MGraph->getVertex()) . "\r\n";
+
+echo 'reverse():'.print_r($MGraph->reserve(),true) . "\r\n";
+echo 'bfs()    :'.print_r($MGraph->bfs(),true) . "\r\n";
+echo 'dfs()    :'.print_r($MGraph->dfs(),true) . "\r\n";
+
+//echo 'floyd() :'.print_r($MGraph->floyd(),true) . "\r\n";
+echo 'dijkstra():'.print_r($MGraph->dijkstra(),true) . "\r\n";
